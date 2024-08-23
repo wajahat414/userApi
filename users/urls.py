36cface5bdb964650945250bbf700ed105c3cd21
wajahat414 <wajahatakram414@gmail.com>
2024-08-23 18:05:23 +0400

    Zip File Download Working

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import UserListCreateView, UserDetailView
+from .views import download_zip
+
 
 urlpatterns = [
     path("users/", UserListCreateView.as_view(), name="user-list-create"),
     path("users/<int:pk>/", UserDetailView.as_view(), name="user-detail"),
+    path("download_zip/", download_zip, name="download_zip"),
 ]
--- users/views.py
+++ users/views.py
@@ -6,6 +6,34 @@ from .models import User
 from .serializers import UserSerializer
 
 
+import os
+import zipfile
+from django.http import HttpResponse
+from django.conf import settings
+
+
+def download_zip(request):
+
+    folder_path = os.path.join(settings.MEDIA_ROOT, "files")
+
+    # Name of the ZIP file
+    zip_filename = "my_files.zip"
+
+    # Create a response object with the appropriate ZIP headers
+    response = HttpResponse(content_type="application/zip")
+    response["Content-Disposition"] = f"attachment; filename={zip_filename}"
+
+    # Create a ZIP file in memory
+    with zipfile.ZipFile(response, "w") as zip_file:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                print(file_path)
+                zip_file.write(file_path, os.path.relpath(file_path, folder_path))
+
+    return response
+
+
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
